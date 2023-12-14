d8313e710d8384c8b57b890e9c90ee67f0cacb20
RatnaGlobalTechAI <chandan.singh@ratnaglobaltech.com>
2023-12-14 10:09:18 +0530

    Deleted deplicate files

--- 4-names.js
+++
@@ -1,8 +0,0 @@
-// local
-
-const secret = 'SUPER SECRET'
-//share
-const chandan = 'Chandan'
-const singh = 'Singh'
-
-module.exports = {singh, chandan}
--- 5-utils.js
+++
@@ -1,8 +0,0 @@
-const helloFunc = (name) => {
-    console.log('Hello there ',name)
-}
-
-helloFunc('Chandan')
-
-//export default
-module.exports = helloFunc
